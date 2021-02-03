72e8e723209ff8be86a81d92344ed945d752e281
Nikolai Paltschikowski <nikolai.pal@gmail.com>
2021-02-03 18:01:18 +0100

    fix(server): add missing index file

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { User, UserDocument } from './schema/user.schema';
+import { User, UserDocument } from './schema';
 
 @Injectable()
 class AuthService {
