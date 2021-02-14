432efe6ac4b53c77b56d2d62ebe0f5317b351a8b
Nikolai Paltschikowski <nikolai.pal@gmail.com>
2021-02-14 17:35:10 +0100

    feat(server): add timestamps to user schema

--- server/src/auth/schema/user.schema.ts
+++ server/src/auth/schema/user.schema.ts
@@ -2,7 +2,10 @@ import { UserDTO } from '@gms/shared';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, ObjectId } from 'mongoose';
 
-@Schema({ versionKey: false })
+@Schema({
+  versionKey: false,
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+})
 class User implements UserDTO {
   @Prop({ required: true, unique: true, index: true })
   handle: string;
