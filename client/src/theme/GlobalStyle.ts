18df7c6cc16ef241ebc2923e4f1d9caa3976719e
Nikolai Paltschikowski <nikolai.pal@gmail.com>
2021-03-13 14:59:51 +0100

    feat(client): add font-style

--- client/src/theme/GlobalStyle.ts
+++ client/src/theme/GlobalStyle.ts
@@ -3,6 +3,11 @@ import { createGlobalStyle } from 'styled-components';
 import { ColorMixin } from './ColorMixin';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Spartan', sans-serif;
+    font-weight: 100;
+  }
+
   body {
     margin: 0;
     ${ColorMixin}
