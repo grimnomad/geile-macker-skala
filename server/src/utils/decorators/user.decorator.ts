0e2e9030735bdf4d0eec09da4c8be59dc170277a
Nikolai Paltschikowski <nikolai.pal@gmail.com>
2021-03-28 15:35:43 +0200

    fix(server): refactor GetUser decorator

--- server/src/utils/decorators/user.decorator.ts
+++ server/src/utils/decorators/user.decorator.ts
@@ -1,12 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
-function GetUser(): ParameterDecorator {
-  return createParamDecorator((_data: unknown, context: ExecutionContext) => {
+const GetUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest<Request>();
 
     return request.user;
-  });
-}
+  }
+);
 
 export { GetUser };
