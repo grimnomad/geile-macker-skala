ebfaa8fb4bac07aaa1b04df804c26f682af8b2d8
Nikolai Paltschikowski <nikolai.pal@gmail.com>
2022-03-31 17:09:33 +0200

    chore(client): update React to v18

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 import { AppProviders } from './AppProviders';
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
     <AppProviders>
       <App />
     </AppProviders>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
