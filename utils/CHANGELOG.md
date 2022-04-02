86392dcb6c12e0ed08ed9dfba187844fbfb5f97c
Nikolai Paltschikowski <nikolai.pal@gmail.com>
2022-04-02 19:46:30 +0200

    feat: add utils package

---
+++ utils/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @gms/utils
+
+## 0.1.0
+
+### Minor Changes
+
+- Initialize package
---
+++ utils/src/noop/noop.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+function noop(..._args: any[]) {}
+
+export { noop };
