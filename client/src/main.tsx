49242721f3dc1453c5e63e7e5d379087504929cb
Nikolai Paltschikowski <nikolai.pal@gmail.com>
2022-04-03 10:46:19 +0200

    fix(client): update dependencies; add casting

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 import { AppProviders } from './AppProviders';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(container as HTMLElement);
 
 root.render(
   <StrictMode>
